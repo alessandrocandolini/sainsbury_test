b7c32ada5503bf9c82ba7e5906a171199756d543
alessandrocandolini <alessandro.candolini@gmail.com>
2016-01-24 22:52:29 +0000

    Global constants

---
+++ my-app/src/main/java/com/sainsburytest/app/main/Constants.java
@@ -0,0 +1,21 @@
+package com.sainsburytest.app.main;
+
+/**
+ * Collection of global constants.
+ * 
+ * @author acando
+ */
+public class Constants {
+
+	/**
+	 * Turn on/off at compilation time the logs 
+	 */
+	public static final Boolean LOG_ENABLED = true;
+	
+	/**
+	 * Generic error message 
+	 */
+	public static final String DEFAULT_ERROR_MESSAGE = "An error occurred";
+	
+	
+}
