5d444aa5cdf5937fd1d88876c57928bacdde2ee8
alessandrocandolini <alessandro.candolini@gmail.com>
2016-01-24 23:19:24 +0000

    App

--- my-app/src/main/java/com/sainsburytest/app/App.java
+++ my-app/src/main/java/com/sainsburytest/app/App.java
@@ -1,13 +1,46 @@
 package com.sainsburytest.app;
 
-/**
- * Hello world!
- *
- */
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.sainsburytest.app.exception.ScrapingException;
+import com.sainsburytest.app.helper.Helper;
+import com.sainsburytest.app.scraper.OutputOperations;
+import com.sainsburytest.app.scraper.ScrapingOperations;
+import com.sainsburytest.app.pojo.InputPojo;
+import com.sainsburytest.app.pojo.ItemPojo;
+import com.sainsburytest.app.pojo.MethodErrorPojo;
+import com.sainsburytest.app.pojo.OutputPojo;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String url = "http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html";
+    	
+    	InputPojo input = new InputPojo();
+    	input.setUrl(url);
+    	
+    	OutputPojo output = new OutputPojo();
+    	
+    	List<ItemPojo> list = null;
+		try {
+			list = ScrapingOperations.consumeListOfItems(input);
+		} catch (ScrapingException e) {
+			MethodErrorPojo methodError = new MethodErrorPojo();
+			methodError.setLocalizedDescription(Constants.DEFAULT_ERROR_MESSAGE);
+			output.setError(methodError);
+		}
+    	
+    	List<ItemPojo> list2 = ScrapingOperations.consumeSingleItemPage(list);
+        
+    	BigDecimal total = Helper.computeTotal(list2);
+    	
+    	output.setResults(list2);
+    	output.setTotal(total);
+    	
+		System.out.println( OutputOperations.serialize(output) );
+		
+    
     }
 }
