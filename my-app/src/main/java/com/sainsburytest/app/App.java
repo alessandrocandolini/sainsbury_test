1d4680ba76b6c616b3b422b6d6573a58066b52bf
alessandrocandolini <alessandro.candolini@gmail.com>
2016-01-25 01:14:47 +0000

    Some annotations

--- my-app/src/main/java/com/sainsburytest/app/App.java
+++ my-app/src/main/java/com/sainsburytest/app/App.java
@@ -9,15 +9,20 @@ public class App
 {
     public static void main( String[] args )
     {
+    	// hardcoded url. can be easily replaced by input arg
     	String url = "http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html";
     	
+    	// build input object
     	ApplicationInputDataPojo input = new ApplicationInputDataPojo();
     	input.setUrl(url);
     	
+    	// construct output object
     	ApplicationOutputDataPojo output = ApplicationOperations.processInputData(input);
     	
+    	// serialize output to json
     	String outputJsonFeed = OutputOperations.serialize(output);
     	
+    	// print to standard output
 		System.out.println( outputJsonFeed );
 		
     
